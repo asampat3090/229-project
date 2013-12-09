de80d417d8ab7eec7f65644cabe98c8407d5ac51
andreesteva <andre.esteva@gmail.com>
2013-12-08 17:33:03 -0800

    Object Oriented Infrastructure Complete - Working 'Run_oo'
    The CellData class has been updated
    The script ‘Run_oo’ is completed and working
    Can now do AND boolean logic on cell type and stimulation type

--- tSNE.m
+++
@@ -1,7 +0,0 @@
-function [reducedVectorSetY,updatedReducedVectorSetY] = tSNE(initialDataX,initialDataY)
-    % initialDataX is a column vector with each row being an x for each
-    % example
-    % initialDataY is a matrix with each row for each example and each
-    % column representing one of the y features.
-    
-end
